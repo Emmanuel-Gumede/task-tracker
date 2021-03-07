7f5b677b57fa377a36ab23a5f4d158cb8ec9761c
Emmanuel Gumede <okuhle.connect@gmail.com>
2021-03-07 18:54:20 +0200

    add static files

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require("express");
 const exphbs = require("express-handlebars");
+const path = require("path");
 
 const tracker = express();
 
+tracker.use(express.static(path.join(__dirname, "public")));
 tracker.engine("hbs", exphbs({ extname: "hbs" }));
 tracker.set("view engine", "hbs");
-
-tracker.get("/", (req, res) => {
-  res.render("home");
-});
 
 tracker.listen(5000, () => {
   console.log("Task Tracker dev server running on http://127.0.0.1:5000");
