d7963b375cfafaa1de0516da98926d03c79a36cd
Emmanuel Gumede <okuhle.connect@gmail.com>
2021-03-07 17:46:22 +0200

    initiated basic express server

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
+
+const express = require("express");
+
+const tracker = express();
+
+tracker.get("/", (req, res) => {
+  res.send("Task Tracker application server");
+});
+
+tracker.listen(5000, () => {
+  console.log("Task Tracker dev server running on http://127.0.0.1:5000");
+});
